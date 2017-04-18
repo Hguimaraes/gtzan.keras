a912b9d527ef745b59df10922bd75317a5bc4447
Hguimaraes <hguimaraes@poli.ufrj.br>
2017-04-18 01:00:11 -0300

    Fixing one-hot encoding of mfcc

--- src/mfcc.py
+++ src/mfcc.py
@@ -1,4 +1,5 @@
 import os
+import keras
 import librosa
 import numpy as np
 
@@ -10,8 +11,8 @@ class MFCC(object):
     self.song_samples = 660000
     self.n_mfcc = 20
     self.file_path = file_path
-    self.genres = ['metal', 'disco', 'classical', 'hiphop', 'jazz', 'country',
-      'pop', 'blues', 'reggae', 'rock']
+    self.genres = {'metal': 0, 'disco': 1, 'classical': 2, 'hiphop': 3, 'jazz': 4,
+     'country': 5, 'pop': 6, 'blues': 7, 'reggae': 8, 'rock': 9}
   
   def getdata(self):
     # Allocate memory for the array of songs
@@ -19,7 +20,7 @@ class MFCC(object):
     genre_data = []
 
     # Read files from the folders
-    for x in self.genres:
+    for x,_ in self.genres.items():
       for root, subdirs, files in os.walk(self.file_path + x):
         for file in files:
           # Read the audio file
@@ -32,6 +33,6 @@ class MFCC(object):
             sr = sr, n_mfcc = self.n_mfcc)
 
           # Append the result to the data structure
-          song_data.append(mfcc)
-          genre_data.append(x)
-    return np.array(song_data), np.array(genre_data)
+          song_data.append(np.transpose(mfcc))
+          genre_data.append(self.genres[x])
+    return np.array(song_data), keras.utils.to_categorical(genre_data, len(self.genres))
