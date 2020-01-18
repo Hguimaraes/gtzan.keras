6c2e705e5066ff9a80cdb245a16bf2f21cc01825
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2020-01-18 12:26:33 -0300

    finishing dl score  procedure

--- src/gtzan/__init__.py
+++ src/gtzan/__init__.py
@@ -1,6 +1,7 @@
 from gtzan.data.make_dataset import make_dataset_dl
 from gtzan.data.make_dataset import make_dataset_ml
 from gtzan.utils import majority_voting
+from gtzan.utils import get_genres
 from joblib import load
 from tensorflow.keras.models import load_model
 
@@ -8,22 +9,22 @@ __all__ = ['AppManager']
 
 
 class AppManager:
-	def __init__(self, args, genres):
-		self.args = args
-		self.genres = genres
+    def __init__(self, args, genres):
+        self.args = args
+        self.genres = genres
 
-	def run(self):
-		if self.args.type == "ml":
-			X = make_dataset_ml(self.args)
-			pipe = load(self.args.model)
-			pred = pipe.predict(X)
-			print("{} is a {} song".format(self.args.song, pred))
+    def run(self):
+        if self.args.type == "ml":
+            X = make_dataset_ml(self.args)
+            pipe = load(self.args.model)
+            pred = get_genres(pipe.predict(X)[0], self.genres)
+            print("{} is a {} song".format(self.args.song, pred))
 
-		else:
-			X = make_dataset_dl(self.args, self.genres)
-			model = load_model(self.args.model)
+        else:
+            X = make_dataset_dl(self.args)
+            model = load_model(self.args.model)
 
-			preds = model.predict(X)
-			votes = majority_voting(preds)
-			print("{} is a {} song".format(self.args.song, votes[0]))
-			print("other possible genres are: {}".format(votes[1:3]))
+            preds = model.predict(X)
+            votes = majority_voting(preds, self.genres)
+            print("{} is a {} song".format(self.args.song, votes[0]))
+            print("other possible genres are: {}".format(votes[1:3]))
--- src/gtzan/utils/__init__.py
+++ src/gtzan/utils/__init__.py
@@ -1,4 +1,20 @@
+import numpy as np
+
 __all__ = ["majority_voting"]
 
-def majority_voting():
-    pass
+
+def majority_voting(scores, dict_genres):
+    preds = np.argmax(scores, axis = 1)
+    values, counts = np.unique(preds, return_counts=True)
+    votes = {k:v for k, v in zip(values, counts)}
+    votes = {k: v for k, v in sorted(votes.items(), key=lambda item: item[1], reverse=True)}
+    votes = list(votes.keys())
+    return [get_genres(x, dict_genres) for x in votes]
+
+
+def get_genres(key, dict_genres):
+    # Transforming data to help on transformation
+    labels = []
+    tmp_genre = {v:k for k,v in dict_genres.items()}
+
+    return tmp_genre[key]
